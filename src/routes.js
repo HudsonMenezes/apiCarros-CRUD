4a3e26f41fd2e3e2d36d166957258f736b0333ae
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-29 15:28:22 -0300

    PUT method done and working

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const CarroController = require('./controllers/CarroController') //para enxergar
 router.get('/carros', CarroController.buscarTodos) // criando endpoint /carros
 router.get('/carro/:codigo', CarroController.buscarUm) // criando novo endpoint
 router.post('/carro', CarroController.inserir)
+router.put('/carro/:codigo', CarroController.alterar)
 
 module.exports = router

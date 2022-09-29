fde315caf255932979b9efba7de3a8f0dee32d2d
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-28 21:24:52 -0300

    server config done and running ok

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: 'variaveis.env' }) // lê o caminho (path) para que esse arquivo seja visto dentro do servidor
+const express = require('express') // cria uma constante para chamar o express
+const cors = require('cors') // cria constante para chamar o cors - permite acesso a recursos para que a gente trabalhe com API, se não tiver, pode dar erro.
+const bodyParser = require('body-parser') // módulo para converter o body da requisição para varios outros formatos
+
+const routes = require('./routes') // mostra para o servidor onde estão as rotas do projeto
+
+const server = express()
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: false }))
+
+server.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`)
+})
